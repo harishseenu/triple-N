97433e5c817e798f93e0a93184ede33d614af938
harishs <harishsrinivasan9182@gmail.com>
2026-02-01 18:45:52 +0530

    Fix static and templates path for Render deployment

--- backend/main.py
+++ backend/main.py
@@ -18,8 +18,15 @@ app.add_middleware(
     same_site="lax"
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+
+
+app.mount(
+    "/static",
+    StaticFiles(directory="backend/static"),
+    name="static"
+)
+
+templates = Jinja2Templates(directory="backend/templates")
 
 # Create tables automatically
 Base.metadata.create_all(bind=engine)
